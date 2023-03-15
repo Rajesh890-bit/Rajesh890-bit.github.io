e71f7c8a84e8e57f3af646dff5d66e029e2730d7
Rajesh Pradhan <pradhan143rajesh@gmail.com>
2023-03-15 11:16:11 +0530

    depoly 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum at
+        sapiente quasi atque sunt doloremque maiores consequatur, provident
+        harum quia? Quam, voluptatibus! Illo ratione impedit sunt eos ab quia
+        asperiores. Ea, quibusdam.
+      </p>
     </div>
   );
 }
